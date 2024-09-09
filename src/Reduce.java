f752d226bbdcbb13eaa0da48d45824baf4c5c63a
eMondri <106029102+eMondri@users.noreply.github.com>
2024-09-09 12:10:07 -0400

    Created Reduce.java, completed README

---
+++ src/Reduce.java
@@ -0,0 +1,16 @@
+public class Reduce {
+    public static void main(String[] args) {
+        int steps = 0;
+        int num = 100;
+        while (num > 0) {
+            if (num % 2 == 0) {
+                num /= 2;
+            }
+            else {
+                num--;
+            }
+            steps++;
+        }
+        System.out.println(steps);
+    }
+}
