2b9503bd05e41f8d9f26524b17d655459353a00e
eMondri <106029102+eMondri@users.noreply.github.com>
2024-09-09 11:53:39 -0400

    completed Task 3.3

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        FizzBuzz.doFizzBuzz(50);
+
+        String[] strArr = {"word1", "word2"};
+        FizzBuzz.main(strArr);
+    }
+}
